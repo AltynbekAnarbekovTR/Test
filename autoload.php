a9ed0621d24b9387266ee71d01a9e3c2b9ee725b
Altynbek312 <altyn_312@mail.ru>
2023-08-24 16:51:59 +0600

    Change db name good-mood2 to good_mood2

--- autoload.php
+++ autoload.php
@@ -21,11 +21,12 @@ spl_autoload_register(
         $classname_parts = explode('\\', $classname);
         $classfile = array_pop($classname_parts) . '.php';
         $namespace = implode(DIRECTORY_SEPARATOR, $classname_parts);
+        echo 'classfile:';
+        dd($classfile);
         if (!array_key_exists($namespace, $classmap)) {
             return;
         }
-        echo 'classfile:';
-        dd($classfile);
+        
         $file = $classmap[$namespace] . DIRECTORY_SEPARATOR . $classfile;
         if (!file_exists($file) && !class_exists($classname)) {
             return;
