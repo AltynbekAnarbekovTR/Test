c7b9b1bbb50ae8ed38950a853c85d76ee0e0ddff
Altynbek312 <altyn_312@mail.ru>
2023-08-24 17:30:20 +0600

    Change db name good-mood2 to good_mood2

--- public/index.php
+++ public/index.php
@@ -1,13 +1,11 @@
 <?php
 
-echo 'Start';
-
-// if (file_exists('../.env')) {
-//     $env = parse_ini_file('../.env');
-//     if ($env["APP_MODE"] === 'development') {
+if (file_exists('../.env')) {
+    $env = parse_ini_file('../.env');
+    if ($env["APP_MODE"] === 'development') {
         require __DIR__ . '/../src/App/functions.php';
-//     };
-// }
+    };
+}
 
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
