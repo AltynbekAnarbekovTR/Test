04b7ae3714932edc36b3ed944e7c7e6f14357b1f
Altynbek312 <altyn_312@mail.ru>
2023-08-24 15:17:15 +0600

    Change hello to normal

--- public/index.php
+++ public/index.php
@@ -1,3 +1,12 @@
 <?php
 
-echo 'Hello Word'; 
+if (file_exists('../.env')) {
+    $env = parse_ini_file('../.env');
+    if ($env["APP_MODE"] === 'development') {
+        require __DIR__ . '/../src/App/functions.php';
+    };
+}
+
+$app = include __DIR__ . '/../src/App/bootstrap.php';
+
+$app->run();
