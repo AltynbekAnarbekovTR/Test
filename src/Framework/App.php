a6ae1b20a4ec6327ca6b3fa1d66ef6f3aa2d8776
Altynbek312 <altyn_312@mail.ru>
2023-08-24 15:41:34 +0600

    Change db name good-mood2 to good_mood2

--- src/Framework/App.php
+++ src/Framework/App.php
@@ -11,6 +11,7 @@ class App
 
   public function __construct(string $containerDefinitionsPath = null)
   {
+    echo 'bootstrap.php';
     $this->router = new Router();
     $this->container = new Container();
 
